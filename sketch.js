7538f62897c6ffce17e4db3430cb2eb2d3375ed9
CoolbatsWin12 <97554534+CoolbatsWin12@users.noreply.github.com>
2022-02-03 19:32:41 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function setup() 
+{
+  createCanvas(400, 400);
+  box1=new Ox(35,100,299,299)
+  box999=new Ox(36,11,199,199)
+}
+
+function draw() 
+{
+  background(220);
+  box1.display()
+  box999.display()
+}
+
